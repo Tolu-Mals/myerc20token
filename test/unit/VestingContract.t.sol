071d869e319a5f104eeaffd1340ee9be13407035
Tolulope Malomo <malomo.tolz@gmail.com>
2026-03-09 09:12:03 +0100

    add testThatScheduleIsCreatedCorrectly

--- test/unit/VestingContract.t.sol
+++ test/unit/VestingContract.t.sol
@@ -25,11 +25,12 @@ contract VestingContractTest is Test {
 
     function testThatScheduleIsCreatedCorrectly() public {
         uint256 startTimestamp = block.timestamp + (1 * 24 * 60 * 60); //start one day from now
-        uint256 endTimestamp = block.timestamp + (7 * 24 * 60 * 60); //start 7 days from now
+        uint256 endTimestamp = block.timestamp + (14 * 24 * 60 * 60); //end 14 days from now
         uint256 cliffTimestamp = block.timestamp + (4 * 24 * 60 * 60); //start 4 days from now
         uint256 totalAmount = 50e18;
 
-        vm.prank(DEFAULT_SENDER);
+        vm.startPrank(DEFAULT_SENDER);
+        vestifyToken.approve(address(vestingContract), totalAmount);
         vestingContract.createVestingSchedule(
             BENEFICIARY,
             startTimestamp,
@@ -37,6 +38,7 @@ contract VestingContractTest is Test {
             cliffTimestamp,
             totalAmount
         );
+        vm.stopPrank();
 
         VestingContract.VestingSchedule
             memory newVestingSchedule = vestingContract.getVestingSchedule(
